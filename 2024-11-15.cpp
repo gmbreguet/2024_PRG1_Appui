fbb587d00c7cf407f4fea8fcec817d573a8a0850
gmbreguet <guy-michel.breguet@heig-vd.ch>
2024-11-15 12:49:28 +0100

    2024-11-15 12:49:28 - GMB

--- 2024-11-15.cpp
+++ 2024-11-15.cpp
@@ -6,7 +6,49 @@
 
 using namespace std;
 
+//--------------------------------------------------
+int somme_chiffres (int nombre) {
+
+   int somme = 0; // int();
+
+   while(nombre) { // nombre > 0
+      somme += nombre % 10;
+      nombre /= 10;
+   }
+   return somme;
+}
+
+
+//--------------------------------------------------
+int somme_chiffres_str (int nombre) {
+
+   // mettre dans un string
+   string nombre_str = to_string(nombre);
+// cout << "nombre_str : " << nombre_str << endl;
+
+   // parcourir tous les chiffres et sommer les chiffres
+   int somme = 0;
+// cout << "boucle for : ";
+   for (size_t i=0; i<nombre_str.size(); ++i) {
+//    cout << nombre_str[i];
+      somme += nombre_str[i] - '0';
+   }
+
+   return somme;
+}
+
+//--------------------------------------------------
 int main() {
+
+   // calculer la somme des chiffres d'un nombre
+   int nombre = 123456; // somme des chiffres 21
+
+
+   // afficher la somme des chiffres
+   cout << "somme      : " << somme_chiffres(nombre) << endl;
+
+   return EXIT_SUCCESS;
+
    //                0123456789 123456789 1234
    string adresse = "route de Cheseaux no 123b";
    size_t debut = adresse.find_first_of    ("0123456789");
